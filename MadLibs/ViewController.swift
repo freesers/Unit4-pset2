b61f8b5cd8ab1b45d818e4dff2bd685307eff81a
Sander de Vries <sanderdv@icloud.com>
2018-11-19 23:36:23 +0100

    Initial Commit

---
+++ MadLibs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MadLibs
+//
+//  Created by Sander de Vries on 19/11/2018.
+//  Copyright © 2018 Sander de Vries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
